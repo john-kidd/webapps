7ff095726e3b8e64e9ed39baf41a11c5efe034fc
John F Kidd <john_kidd@outlook.com>
2016-10-09 18:27:31 +0100

    completed contact us page
    also tidied up gallery code and got rid of react warning by adding keys
    to child components

--- react/src/components/contact/ContactPage.js
+++ react/src/components/contact/ContactPage.js
@@ -1,6 +1,45 @@
 import React from 'react';
+import Moment from 'moment';
 
 class ContactPage extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            email: {
+                action: '',
+                name: '',
+                message: ''
+            }
+        };
+
+        this.nameDidChange = this.nameDidChange.bind(this);
+        this.messageDidChange = this.messageDidChange.bind(this);
+    }
+
+    updateAction() {
+        const NEW_LINE = '%0D%0A%0D%0A';
+        const email = this.state.email;
+        const name = `Name: ${email.name}`;
+        const dateTime = `Date: ${Moment(new Date()).format('ddd MMM YYYY HH:MM')}`;
+        email.action = `mailto:john_kidd@outlook.com?subject=Quotation&body=${name}${NEW_LINE}${dateTime}${NEW_LINE}${email.message}`; 
+        this.setState({email: email});     
+    }
+
+    nameDidChange(event) {
+        const email = this.state.email;
+        email.name = event.target.value;
+        this.setState({email: email});
+        this.updateAction();     
+    }
+
+    messageDidChange(event) {
+        const email = this.state.email;
+        email.message = event.target.value;
+        this.setState({email: email});
+        this.updateAction();     
+    }
+
     render() {
         return (
             <div>
@@ -9,34 +48,50 @@ class ContactPage extends React.Component {
                     <h4>Please get in touch for a free quote!</h4>
                     <div>Interested in our services?  Get in touch with us via the form below and we'll get back to you as soon as possible.</div>
                     <br/>
-                    <form className="col-md-12" action="mailto:john_kidd@outlook.com" method="post" enctype="text/plain">
+                    <form 
+                        className="col-md-12">
                         <div className="form-group">
-                            <label for="name">Name:</label>
-                            <input type="name" className="form-control" id="name"></input>
+                            <label htmlFor="name">Name:</label>
+                            <input 
+                                type="name" 
+                                className="form-control" 
+                                id="name"
+                                onChange={this.nameDidChange}></input>
                         </div>
                         <div className="form-group">
-                            <label for="email">Email address:</label>
-                            <input type="email" className="form-control" id="email"></input>
-                        </div>
-                        <div class="form-group">
-                            <label for="body">Message:</label>
-                            <textarea type="body" className="form-control" id="body" rows="10"></textarea>
+                            <label htmlFor="body">Message:</label>
+                            <textarea 
+                                type="body" 
+                                className="form-control" 
+                                id="body" 
+                                rows="10"
+                                cols="10"
+                                onChange={this.messageDidChange} 
+                                defaultValue={this.state.email.body}>
+                            </textarea>
                         </div>
                         <br/>
-                        <button type="submit" className="btn btn-primary btn-md">Request Quote</button>
+                        <a 
+                            href={this.state.email.action} 
+                            className="btn btn-primary btn-md">
+                            Request a Quote
+                        </a>
                     </form>
                 </section>
                 <section className="col-md-4">
                     <h4 className="info-section">Contact Information</h4>
                     <div className="info-section">
                         <br/><strong>Val Home Services</strong> 
-                        <br/>Phone: 07713 758164
-                        <br/>Email: kvlatko161@gmail.com
+                        <br/>Phone: 07751 159465
+                        <br/>Email: valhomeservice@gmail.com
                         
                         <br/><br/><strong>Opening Hours</strong>
                         <br/>Mon-Fri: 9-5pm
                         <br/>Sat: 10-4pm
                         <br/>Sun: Closed
+                        
+                        <br/><br/><strong>Out of Hours</strong>
+                        <br/>Phone: 07713 758164
 
                         <br/><br/><strong>Address</strong>
                         <br/>St. Albans,
--- react/src/components/gallery/Picture.js
+++ react/src/components/gallery/Picture.js
@@ -1,31 +1,37 @@
 import React from 'react';
 
+class ListItemWrapper extends React.Component {
+  render() {
+    return <div className="row">
+        <div className="col-md-3">
+            <img src={this.props.pictures[0].src} className="thumbnail picture" />
+            <h5>{this.props.pictures[0].title}</h5>
+            <h6>{this.props.pictures[0].subTitle}</h6>
+        </div>
+        <div className="col-md-3">
+            <img src={this.props.pictures[1].src} className="thumbnail picture" />
+            <h5>{this.props.pictures[1].title}</h5>
+            <h6>{this.props.pictures[1].subTitle}</h6>
+        </div>
+        <div className="col-md-3">
+            <img src={this.props.pictures[2].src} className="thumbnail picture" />
+            <h5>{this.props.pictures[2].title}</h5>
+            <h6>{this.props.pictures[2].subTitle}</h6>
+        </div>
+        <div className="col-md-3">
+            <img src={this.props.pictures[3].src} className="thumbnail picture" />
+            <h5>{this.props.pictures[3].title}</h5>
+            <h6>{this.props.pictures[3].subTitle}</h6>
+        </div>
+    </div>;
+  }
+}
+
 export default React.createClass({
     render: function() {
-        const gallery = this.props.gallery.map((pictures) => {
+        const gallery = this.props.gallery.map((pictures, index) => {
             return (
-                <div className="row">
-                    <div className="col-md-3">
-                        <img src={pictures[0].src} className="thumbnail picture" />
-                        <h5>{pictures[0].title}</h5>
-                        <h6>{pictures[0].subTitle}</h6>
-                    </div>
-                    <div className="col-md-3">
-                        <img src={pictures[1].src} className="thumbnail picture" />
-                        <h5>{pictures[1].title}</h5>
-                        <h6>{pictures[1].subTitle}</h6>
-                    </div>
-                    <div className="col-md-3">
-                        <img src={pictures[2].src} className="thumbnail picture" />
-                        <h5>{pictures[2].title}</h5>
-                        <h6>{pictures[2].subTitle}</h6>
-                    </div>
-                    <div className="col-md-3">
-                        <img src={pictures[3].src} className="thumbnail picture" />
-                        <h5>{pictures[3].title}</h5>
-                        <h6>{pictures[3].subTitle}</h6>
-                    </div>
-                </div>
+                <ListItemWrapper key={index} pictures={pictures} />
             );
         });
         return <div>{gallery}</div>;
