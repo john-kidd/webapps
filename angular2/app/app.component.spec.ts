08d76b06d551d5884077947004a003be162310f1
John F Kidd <john_kidd@outlook.com>
2016-10-22 17:16:35 +0100

    fixed test

--- angular2/app/app.component.spec.ts
+++ angular2/app/app.component.spec.ts
@@ -5,33 +5,20 @@ import { TestBed }      from '@angular/core/testing';
 
 import { By }           from '@angular/platform-browser';
 
-////////  SPECS  /////////////
+import { RouterTestingModule } from '@angular/router/testing';
 
-/// Delete this
-describe('Smoke test', () => {
-  it('should run a passing test', () => {
-    expect(true).toEqual(true, 'should pass');
-  });
-});
+////////  SPECS  /////////////
 
 describe('AppComponent with TCB', function () {
   beforeEach(() => {
-    TestBed.configureTestingModule({declarations: [AppComponent]});
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      imports: [ RouterTestingModule ]
+    });
   });
 
-  it('should instantiate component', () => {
+  it('application starts', () => {
     let fixture = TestBed.createComponent(AppComponent);
     expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
-  });
-
-  it('should have expected <h1> text', () => {
-    let fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-
-    let h1 = fixture.debugElement.query(el => el.name === 'h1').nativeElement;  // it works
-
-        h1 = fixture.debugElement.query(By.css('h1')).nativeElement;            // preferred
-
-    expect(h1.innerText).toMatch(/angular app/i, '<h1> should say something about "Angular App"');
   });
 });
