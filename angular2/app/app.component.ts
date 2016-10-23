2cb357dbcbafcdcd443df6ca29c84c64a485b17d
John F Kidd <john_kidd@outlook.com>
2016-10-23 13:36:13 +0100

    added bootstrap

--- angular2/app/app.component.ts
+++ angular2/app/app.component.ts
@@ -2,12 +2,27 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `
-    <nav>
-      <a routerLink="/" routerLinkActive="active">Home</a>
-      <a routerLink="/about" routerLinkActive="active">About</a>
-    </nav>
-    <router-outlet></router-outlet>
-  `
+    template: 
+      `
+        <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+          <div class="container-fluid">
+            <div class="navbar-header">
+              <a routerLink="/" routerLinkActive="active" class="navbar-brand">Home</a>
+              <a routerLink="/about" routerLinkActive="active" class="navbar-brand">About</a>
+            </div>
+            <div class="collapse navbar-collapse">
+              <ul class="nav navbar-nav navbar-right">
+                
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="container-fluid">
+          <div class="content">
+            <router-outlet></router-outlet>
+          </div>
+        </div>  
+      `
 })
 export class AppComponent { }
